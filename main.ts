9f1c55d225a8dffcce607b8f36dd2d9a948f9358
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2024-05-28 20:15:54 +0100

    use while loop

--- main.ts
+++ main.ts
@@ -1,11 +1,17 @@
-input.onButtonPressed(Button.B, function () {
-    basic.clearScreen()
+function Calc_Total () {
     Total = 1 * pins.digitalReadPin(DigitalPin.P1) + 2 * pins.digitalReadPin(DigitalPin.P2) + (4 * pins.digitalReadPin(DigitalPin.P5) + 8 * pins.digitalReadPin(DigitalPin.P8))
-    basic.showNumber(Total)
+}
+input.onButtonPressed(Button.B, function () {
+	
 })
 let Total = 0
-basic.showIcon(IconNames.Yes)
+basic.showIcon(IconNames.Skull)
 Total = 0
+let Old_Total = -1
 basic.forever(function () {
-	
+    Calc_Total()
+    while (Total != Old_Total) {
+        basic.showNumber(Total)
+        Old_Total = Total
+    }
 })
